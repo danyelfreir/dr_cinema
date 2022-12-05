a4ee8c2457ec1df3ca6b64d658f3070fa40e195b
Helgi <helgihak@gmail.com>
2022-12-05 08:24:20 +0000

    init folders

---
+++ drcinema/src/components/CinemaList/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+
+const CinemaList = () => {
+  return (
+    <View>
+      <Text>CinemaList</Text>
+    </View>
+  );
+};
+
+export default CinemaList;
---
+++ drcinema/src/styles/index.js
@@ -0,0 +1,8 @@
+export default {
+  red: "#FE3D30",
+  gray: "#9D9D9D",
+  white: "#fff",
+  blackAlpha: "rgba(0, 0, 0, 0.75)",
+  blackAlpha2: "rgba(0, 0, 0, 0.5)",
+  black: "#000",
+};
