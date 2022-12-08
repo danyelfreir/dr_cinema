7713d696a9955a4a86c653c64838dd3b63371efb
Daníel Freyr Grétarsson <danielfgretars@gmail.com>
2022-12-08 08:49:45 +0000

    lll

--- drcinema/src/components/MovieInfo/index.jsx
+++ drcinema/src/components/MovieInfo/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import MovieInfoChild from '../MovieInfoChild/index.js';
 import styles from './styles.js';
 
 const MovieInfo = ({ movie }) => {
@@ -7,42 +8,23 @@ const MovieInfo = ({ movie }) => {
     <View style={styles.informationContainer}>
       <Image style={styles.poster} source={{ uri: movie.poster }} />
       <View style={styles.informationTextContainer}>
-        <View style={styles.informationTextPair}>
-          <Text style={styles.informationTextKey}>Lengd:</Text>
-          <Text style={styles.informationTextValue}>
-            {movie.durationMinutes} mín
-          </Text>
-        </View>
-        <View style={styles.informationTextPair}>
-          <Text style={styles.informationTextKey}>Aldurstakmark:</Text>
-          <Text style={styles.informationTextValue}>{movie.certificateIS}</Text>
-        </View>
-        <View style={styles.informationTextPair}>
-          <Text style={styles.informationTextKey}>Leikstjóri:</Text>
-          {movie.directors_abridged.map((director) => (
-            <Text style={styles.informationTextValue}>{director.name}</Text>
-          ))}
-        </View>
-        <View style={styles.informationTextPair}>
-          <Text style={styles.informationTextKey}>Leikarar:</Text>
-          <Text style={styles.informationTextValue}>
-            {movie.actors_abridged.map(
-              (actor, index) =>
-                `${actor.name}${
-                  index < movie.actors_abridged.length - 1 ? ', ' : ''
-                }`
-            )}
-          </Text>
-        </View>
-        <View style={styles.informationTextPair}>
-          <Text style={styles.informationTextKey}>Flokkar:</Text>
-          <Text style={styles.informationTextValue}>
-            {movie.genres.map(
-              (genre, index) =>
-                `${genre.Name}${index < movie.genres.length - 1 ? ', ' : ''}`
-            )}
-          </Text>
-        </View>
+        <MovieInfoChild
+          infoKey={'Lengd:'}
+          infoValue={`${movie.durationMinutes} mín`}
+        />
+        <MovieInfoChild
+          infoKey={'Aldurstakmark:'}
+          infoValue={movie.certificateIS}
+        />
+        <MovieInfoChild
+          infoKey={'Leikstjóri:'}
+          infoValueArray={movie.directors_abridged}
+        />
+        <MovieInfoChild
+          infoKey={'Leikarar:'}
+          infoValueArray={movie.actors_abridged}
+        />
+        <MovieInfoChild infoKey={'Flokkar:'} infoValueArray={movie.genres} />
       </View>
     </View>
   );
